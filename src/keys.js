a59c2153c88a56835c4b2ed8940e818a34533308
irinakud <irbaska@gmail.com>
2021-04-14 18:16:22 +1000

    Added sound files to repo

--- src/keys.js
+++ src/keys.js
@@ -1,57 +1,67 @@
+import audio1 from './Heater-1.mp3'
+import audio2 from './Heater-2.mp3'
+import audio3 from './Heater-3.mp3'
+import audio4 from './Heater-4_1.mp3'
+import audio5 from './Heater-6.mp3'
+import audio6 from './Dsc_Oh.mp3'
+import audio7 from './Kick_n_Hat.mp3'
+import audio8 from './RP4_KICK_1.mp3'
+import audio9 from './Cev_H2.mp3'
+
 export const keys = [
     {
       keyCode: 81,
       keyTrigger: 'Q',
       id: 'Heater-1',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+      url: audio1
     },
     {
       keyCode: 87,
       keyTrigger: 'W',
       id: 'Heater-2',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+      url: audio2
     },
     {
       keyCode: 69,
       keyTrigger: 'E',
       id: 'Heater-3',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+      url: audio3
     },
     {
       keyCode: 65,
       keyTrigger: 'A',
       id: 'Heater-4',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+      url: audio4
     },
     {
       keyCode: 83,
       keyTrigger: 'S',
       id: 'Clap',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+      url: audio5
     },
     {
       keyCode: 68,
       keyTrigger: 'D',
       id: 'Open-HH',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+      url: audio6
     },
     {
       keyCode: 90,
       keyTrigger: 'Z',
       id: "Kick-n'-Hat",
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+      url: audio7
     },
     {
       keyCode: 88,
       keyTrigger: 'X',
       id: 'Kick',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+      url: audio8
     },
     {
       keyCode: 67,
       keyTrigger: 'C',
       id: 'Closed-HH',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+      url: audio9
     }
   ];
   
